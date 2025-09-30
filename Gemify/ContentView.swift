80583d90f643b652221cd4768bdf6b19e7a331e9
canifty <can-dindar@outlook.com>
2025-09-30 10:12:39 +0200

    Initial Commit

---
+++ Gemify/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Gemify
+//
+//  Created by Can Dindar on 30/09/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
