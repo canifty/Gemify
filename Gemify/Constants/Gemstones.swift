c4164d5cfd849ea45b9b3e7847c93418814d645c
Francisco Mestizo <panchitomestizo@gmail.com>
2025-09-30 11:55:52 +0200

    feat(gemstones): Add logic to identify gemstones

---
+++ Gemify/Constants/Gemstones.swift
@@ -0,0 +1,25 @@
+//
+//  Gemstones.swift
+//  Gemify
+//
+//  Created by Francisco Mestizo on 30/09/25.
+//
+
+import Foundation
+
+let allGemstones: [Gemstone] = [Gemstone(name: "Diamond", recipe: [.carbon]),
+                                     Gemstone(name: "Amethyst", recipe: [.oxigen, .silicon]),
+                                     Gemstone(name: "Ruby", recipe: [.aluminum, .oxigen]),
+                                     Gemstone(name: "Sapphire", recipe: [.aluminum, .oxigen]),
+                                     Gemstone(name: "Pearl", recipe: [.calcium, .carbon, .oxigen]),
+                                     Gemstone(name: "Opal", recipe: [.hydrogen, .oxigen, .silicon]),
+                                     Gemstone(name: "Peridot", recipe: [.magnesium, .oxigen, .silicon]),
+                                     Gemstone(name: "Alexandrite", recipe: [.aluminum, .beryllium, .oxigen]),
+                                     Gemstone(name: "Emerald", recipe: [.aluminum, .beryllium, .oxigen, .silicon]),
+                                     Gemstone(name: "Aquamarine", recipe: [.aluminum, .beryllium, .oxigen, .silicon]),
+                                     Gemstone(name: "Garnet", recipe: [.aluminum, .magnesium, .oxigen, .silicon]),
+                                     Gemstone(name: "Spinel", recipe: [.aluminum, .magnesium, .oxigen]),
+                                     Gemstone(name: "Topaz", recipe: [.aluminum, .fluorine, .hydrogen, .oxigen, .silicon]),
+                                Gemstone(name: "Turquoise", recipe: [.aluminum, .hydrogen, .oxigen, .phosphorus, .copper]),
+                                     Gemstone(name: "Zircon", recipe: [.oxigen, .silicon, .zirconium]),
+                                     Gemstone(name: "Tourmaline", recipe: [.aluminum, .boron, .fluorine, .hydrogen, .lithium, .oxigen, .silicon, .sodium])]
---
+++ Gemify/Utils/Gems.swift
@@ -0,0 +1,29 @@
+//
+//  GemCreator.swift
+//  Gemify
+//
+//  Created by Francisco Mestizo on 30/09/25.
+//
+
+import Foundation
+
+enum Element {
+    case oxigen, aluminum, silicon, hydrogen, magnesium, carbon, beryllium, calcium, fluorine, phosphorus, zirconium, boron, lithium, sodium, copper
+}
+
+struct Gemstone {
+    let name: String
+    let recipe: Set<Element>
+}
+
+func createGem(from elems: [Element]) -> Gemstone? {
+    let inputElementsSet = Set(elems)
+    return allGemstones.first { $0.recipe == inputElementsSet }
+}
+
+func formedGemstones(from elements: [Element]) -> [String] {
+    let inputSet = Set(elements)
+    return allGemstones
+        .filter { $0.recipe.isSubset(of: inputSet) }
+        .map { $0.name }
+}
