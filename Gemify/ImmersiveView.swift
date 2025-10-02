e7833409306c43dd94fcdb8b67d40da9786312e8
canifty <can-dindar@outlook.com>
2025-10-02 11:32:41 +0200

    feat(collision) detect object mesh itself

--- Gemify/ImmersiveView.swift
+++ Gemify/ImmersiveView.swift
@@ -1,30 +1,37 @@
-//
-//  CubeImmersiveView.swift
-//  Gemify
-//
-//  Created by Can Dindar on 30/09/25.
-//
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
-    
+
     var body: some View {
         RealityView { content in
-            if let entity = try? await Entity(named: "Diamondtest", in: realityKitContentBundle) {
-                
-                entity.components.set(GestureComponent())
-                entity.components.set(InputTargetComponent())
-                entity.components.set(CollisionComponent(
-                    shapes: [ShapeResource.generateBox(size: [0.1, 0.1, 0.1])],
-                    mode: .default,
-                    filter: CollisionFilter(group: .all, mask: .all)))
-                
-                content.add(entity)
+            // Load and add Cone
+            if let cone = try? await Entity(named: "Cone", in: realityKitContentBundle) {
+                cone.components.set(GestureComponent())
+                cone.components.set(InputTargetComponent())
+                let meshBounds = cone.visualBounds(relativeTo: nil)
+                let size = meshBounds.extents // actual mesh size
+                cone.components.set(CollisionComponent(
+                    shapes: [ShapeResource.generateBox(size: size)]
+                ))
+                content.add(cone)
             } else {
-                print("no object")
+                print("Cone not found!!")
+            }
+
+            // Load and add Diamond
+            if let diamond = try? await Entity(named: "Diamondtest", in: realityKitContentBundle) {
+                diamond.components.set(GestureComponent())
+                diamond.components.set(InputTargetComponent())
+                let meshBounds = diamond.visualBounds(relativeTo: nil)
+                let size = meshBounds.extents // actual mesh size
+                diamond.components.set(CollisionComponent(
+                    shapes: [ShapeResource.generateBox(size: size)]
+                ))
+                content.add(diamond)
+            } else {
+                print("Diamond not found!!")
             }
         }
         update: { content in
