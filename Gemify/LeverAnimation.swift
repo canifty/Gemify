95871af3d15e31e0583712841615849eb65e01fd
canifty <can-dindar@outlook.com>
2025-10-05 16:23:04 +0200

    feat(LeverAnimation) add LeverAnimation view

---
+++ Gemify/LeverAnimation.swift
@@ -0,0 +1,66 @@
+//
+//  LeverAnimation.swift
+//  Gemify
+//
+//  Created by Can Dindar on 03/10/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+// TODO: WHEN ANIMATION ENDS DISPLAY THE GEM
+
+struct LeverAnimation: View {
+    
+    @State private var isAnimating = false
+    
+    var body: some View {
+        
+        RealityView { content in
+            if let lever = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                lever.components.set(GestureComponent())
+                lever.components.set(InputTargetComponent())
+                let meshBounds = lever.visualBounds(relativeTo: nil)
+                let size = meshBounds.extents // actual mesh size
+                lever.components.set(CollisionComponent(
+                    shapes: [ShapeResource.generateBox(size: size)]
+                ))
+                content.add(lever)
+                
+            }
+        }
+        update: { content in
+            
+            guard let lever = content.entities.first else { return }
+            
+            if isAnimating {
+                lever.availableAnimations.forEach { animation in
+                    lever.playAnimation(animation, transitionDuration: 3)
+                }
+            } else {
+                lever.stopAllAnimations()
+            }
+        }
+        .installGestures()
+        
+        //        when tapped on the lever start/stop the animation
+        .gesture(
+            SpatialTapGesture()
+                .targetedToAnyEntity()
+                .onEnded { _ in
+                    isAnimating.toggle()
+                }
+        )
+    }
+}
+//    @State var subscriptions: [EventSubscription] = []
+//let sub = content.subscribe(to: AnimationEvents.PlaybackCompleted.self,
+//                            on: lever) { event in
+//    print("Animation completed!")
+// subscriptions.append(sub)
+//}
+
+#Preview {
+    LeverAnimation()
+}
