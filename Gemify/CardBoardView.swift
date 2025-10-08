15ea53a37ba6e60176762bb9e12e11617953a427
canifty <can-dindar@outlook.com>
2025-10-08 14:38:17 +0200

    fix(Gems) gems resized

---
+++ Gemify/CardBoardView.swift
@@ -0,0 +1,42 @@
+////
+////  CardBoardView.swift
+////  Gemify
+////
+////  Created by Can Dindar on 08/10/25.
+////
+//
+//import SwiftUI
+//
+//struct CardBoardView: View {
+//    
+//    var imageName: String
+//    
+//    var body: some View {
+//        
+//        RealityView { content in
+//            if let texture = try? await TextureResource(named: imageName) {
+//                
+//                let shapeMesh = MeshResource.generateBox(size: 0.05)
+//                
+//                var material = UnlitMaterial()
+//                material.color = PhysicallyBasedMaterial
+//                    .BaseColor(texture: .init(texture))
+//                
+//                let model = ModelEntity(mesh: shapeMesh, materials: [material])
+//                model.components.set(GestureComponent())
+//                model.components.set(InputTargetComponent())
+//                let meshBounds = model.visualBounds(relativeTo: nil)
+//                let size = meshBounds.extents // actual mesh size
+//                model.components.set(CollisionComponent(
+//                    shapes: [ShapeResource.generateBox(size: size)]
+//                ))
+//                content.add(model)
+//            }
+//        }
+//        .installGestures()
+//    }
+//}
+//
+//#Preview {
+//    CardBoardView()
+//}
